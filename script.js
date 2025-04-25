fc0c92612547a71f490a9d32c1b1f6f3d32ef070
Tim <january1073@yahoo.com>
2025-04-25 08:11:34 +0000

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('https://api.github.com/repos/january1073/january1073.github.io/commits/main')
+    .then(r => r.json())
+    .then(data => {
+        const lastCommit = new Date(data.commit.committer.date);
+        document.getElementById('last-updated').textContent = 
+            lastCommit.toISOString().split('T')[0];
+    });
